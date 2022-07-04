e3ad93e8eb29c6efc06e9b1602a126d27e1fa662
JohnnyGaona <favian--1@hotmail.com>
2022-07-04 12:07:01 -0500

    Actualizar

--- routes/buildpc.py
+++ routes/buildpc.py
@@ -9,16 +9,18 @@ cursor = conexion.cursor()
 @build.route("/viewbuild")
 def muestra ():
     #Seleccion de la targeta madre 
-    cursor.execute("SELECT id, nombre, precio FROM producto where id_categoria='3'")
+    cursor.execute("SELECT id, nombre, precio, stock FROM producto where id_categoria='3'")
     rows1 = cursor.fetchone()
     x=0
+    print("Targeta madre.....")
     while rows1:
-        x = x+1
-        print(x,rows1)
-        if x == 1:
-            lista = [(x,rows1)]
-        else:  
-            lista += [(x,rows1)] 
+        if rows1[3] !=0:
+            x = x+1
+            print(x,rows1)
+            if x == 1:
+                lista = [(x,rows1)]
+            else:  
+                lista += [(x,rows1)] 
 
         rows1 = cursor.fetchone()
     
@@ -32,36 +34,41 @@ def muestra ():
   
     
     #Seleccion de procesador
-    cursor.execute("SELECT  producto.id, nombre, precio FROM compatibilidad_producto INNER JOIN producto ON compatibilidad_producto.producto_compatible = producto.id WHERE id_producto = ? AND id_categoria = 1",tarjeta)
+    cursor.execute("SELECT  producto.id, nombre, precio, stock FROM compatibilidad_producto INNER JOIN producto ON compatibilidad_producto.producto_compatible = producto.id WHERE id_producto = ? AND id_categoria = 1",tarjeta)
     rows2 = cursor.fetchone()
     x=0
+    print("Procesador.....")
     while rows2:
-        x = x+1
-        print(x,rows2)
-        if x == 1:
-            lista2 = [(x,rows2)]
-        else:  
-            lista2 += [(x,rows2)] 
+        if rows2[3] != 0:
+            x = x+1
+            print(x,rows2)
+            if x == 1:
+                lista2 = [(x,rows2)]
+            else:  
+                lista2 += [(x,rows2)] 
 
         rows2 = cursor.fetchone()
 
     inp = input("Ingrese la opcion a elegir: ")
-    for m in lista2:
-        sublista = m
-        if sublista[0] == int(inp):
-            pc += [sublista[1]]
+    if lista2: 
+        for m in lista2:
+            sublista = m
+            if sublista[0] == int(inp):
+                pc += [sublista[1]]
 
     #Seleccion de memoria ram
-    cursor.execute("SELECT  producto.id, nombre, precio FROM compatibilidad_producto INNER JOIN producto ON compatibilidad_producto.producto_compatible = producto.id WHERE id_producto = ? AND id_categoria = 4",tarjeta)
+    cursor.execute("SELECT  producto.id, nombre, precio, stock FROM compatibilidad_producto INNER JOIN producto ON compatibilidad_producto.producto_compatible = producto.id WHERE id_producto = ? AND id_categoria = 4",tarjeta)
     rows3 = cursor.fetchone()
     x=0
+    print("Memoria RAM....")
     while rows3:
-        x = x+1
-        print(x,rows3)
-        if x == 1:
-            lista3 = [(x,rows3)]
-        else:  
-            lista3 += [(x,rows3)] 
+        if rows3[3] !=0:
+            x = x+1
+            print(x,rows3)
+            if x == 1:
+                lista3 = [(x,rows3)]
+            else:  
+                lista3 += [(x,rows3)] 
 
         rows3 = cursor.fetchone()
     inp = input("Ingrese la opcion a elegir: ")
@@ -71,16 +78,18 @@ def muestra ():
             pc += [sublista[1]]
 
     #Seleccion de disco duro
-    cursor.execute("SELECT  producto.id, nombre, precio FROM compatibilidad_producto INNER JOIN producto ON compatibilidad_producto.producto_compatible = producto.id WHERE id_producto = ? AND id_categoria = 5",tarjeta)
+    cursor.execute("SELECT  producto.id, nombre, precio,stock FROM compatibilidad_producto INNER JOIN producto ON compatibilidad_producto.producto_compatible = producto.id WHERE id_producto = ? AND id_categoria = 5",tarjeta)
     rows4 = cursor.fetchone()
     x=0
+    print("Almacenamiento Interno......")
     while rows4:
-        x = x+1
-        print(x,rows4)
-        if x == 1:
-            lista4 = [(x,rows4)]
-        else:  
-            lista4 += [(x,rows4)] 
+        if rows4[3] !=0:
+            x = x+1
+            print(x,rows4)
+            if x == 1:
+                lista4 = [(x,rows4)]
+            else:  
+                lista4 += [(x,rows4)] 
 
         rows4 = cursor.fetchone()
 
@@ -95,9 +104,12 @@ def muestra ():
     print(" ")
     print("Componentes de la PC")
     total=0
+    x=0
     for p in pc:
+        x+=1
+        print(x,p)
         total = total + p[2]
-        
+    print("El coste total por el equipo es de : ",total) 
     inp = input("Desea ordenar el producto: (si/1)(no/0): ")
     
     if inp == '0':
@@ -117,10 +129,9 @@ def muestra ():
                 direccion = cursor.fetchone()
             
             fecha = datetime.now()
-            cursor.execute('insert into orden(id_comprador, monto, direc_envio, estado_envio, fecha_envio) values(?,?,?,?,?);', (ci,total,direccion,estado,fecha))
+            cursor.execute('insert into orden(id_comprador, monto, direc_envio, estado_envio, fecha_envio) values(?,?,?,?,?);', (ci,total,direccion[0],estado,fecha))
             conexion.commit()
             print("Orden Realizada....")
-            #Falta descontar los productos de la base de datos
             for p in pc:
                 cursor.execute("select stock from producto where id=?",p[0])
                 stock = cursor.fetchone()
